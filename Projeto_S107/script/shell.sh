6818dc2e4b6b2b62510393f4d33be6ec8838040f
LucaFelipe <luca.felipe@ges.inatel.br>
2023-10-06 19:08:03 -0300

    Testando postgres

--- Projeto_S107/script/shell.sh
+++ Projeto_S107/script/shell.sh
@@ -5,4 +5,3 @@ echo "Instalando as dependências do JAVA e PostgreSQL"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
-# posso fazer o que eu quiser aqui
