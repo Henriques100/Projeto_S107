42813155b5170180a87ff4aa346fe1ded80c919c
Henriques100 <raphael.h@ges.inatel.br>
2023-10-06 18:24:33 -0300

    removendo docker

--- Projeto_S107/script/shell.sh
+++ Projeto_S107/script/shell.sh
@@ -1,7 +1,7 @@
 ls
 cd Projeto-BD-POO
 ls
-echo "AAAHAHAHAAA"
+echo "Instalando as dependências do JAVA e PostgreSQL"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
