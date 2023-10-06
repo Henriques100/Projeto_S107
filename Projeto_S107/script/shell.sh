174b816fe7ddcd60297c4b81bce81bb8af4ae118
LucaFelipe <luca.felipe@ges.inatel.br>
2023-10-06 19:18:39 -0300

    Atualizando o pipeline

--- Projeto_S107/script/shell.sh
+++ Projeto_S107/script/shell.sh
@@ -1,7 +1,8 @@
 ls
 cd Projeto-BD-POO
 ls
-echo "Instalando as dependências do JAVA e PostgreSQL"
+echo "Instalando as dependências do PostgreSQL"
+echo "Instalando as dependências do Java"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Mandando e-mail com mail do linux" | mail -s "a subject" ${EMAIL_LIST}
